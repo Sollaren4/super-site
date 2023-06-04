75ea6660a12b3b4eb87b5ca540ba90a425ba3a29
sollaren4 <sollaren4@gmail.com>
2023-06-04 11:33:50 +0300

    Create simple questions and users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+	# чтоб был доступен во всех шаблонах
+	helper_method :current_user
+
+	private
+
+	def current_user
+		# не авторизированный - nil
+		# первый раз - запрос в базу
+		# дальше уже будет запись в переменной, запрос не понадобится
+		@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+	end
 end
